9d4f1b9c5d0d70d3b52e5f7f0730c24e656bc519
wylin <wylin.hit@gmail.com>
2018-07-12 17:49:57 -0700

    create mirror.sh

--- install.sh
+++ install.sh
@@ -14,8 +14,7 @@ cp src/init-local.el ~/.emacs.d/lisp/
 cp -r src/wylin-emacs ~/.emacs.d/lisp/
 
 # 修改为国内的镜像，提升数据
-sed 's/\/\/elpa.gnu.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/gnu/g;s/\/\/melpa.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/melpa/g;s/\/\/orgmode.org\/elpa/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/org/g' ~/.emacs.d/lisp/init-elpa.el > init-elpa.el
-mv init-elpa.el ~/.emacs.d/lisp/init-elpa.el
+./mirror.sh
 cd ~/.emacs.d/
 ./test-startup.sh
 
---
+++ mirror.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 修改为国内的镜像，提升数据
+sed 's/\/\/elpa.gnu.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/gnu/g;s/\/\/melpa.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/melpa/g;s/\/\/orgmode.org\/elpa/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/org/g' ~/.emacs.d/lisp/init-elpa.el > init-elpa.el
+mv init-elpa.el ~/.emacs.d/lisp/init-elpa.el
+
+
+
