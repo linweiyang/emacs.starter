5af1e7b7ce6c8835c155ffd5dbc7beae19afae0f
wylin <wylin.hit@gmail.com>
2019-10-19 15:04:23 +0800

    忽略org执行代码时的确认

--- src/wylin-emacs/init-wylin-org.el
+++ src/wylin-emacs/init-wylin-org.el
@@ -4,6 +4,9 @@
 (add-hook 'org-mode-hook
           (lambda () (org-bullets-mode 1)))
 
+;; 忽略org执行代码时的确认
+(setq org-confirm-babel-evaluate nil)
+
 ;; 使用xelatex编译org，并转成pdf
 ;;(setq org-latex-pdf-process (quote ("xelatex -interaction nonstopmode -output-directory %o %f")))
 
