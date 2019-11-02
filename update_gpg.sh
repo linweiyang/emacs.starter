457692ddb2c1ca0e75b14c19a9f59f04b1df5a3c
wylin <wylin.hit@gmail.com>
2019-11-03 00:05:43 +0800

    判断emacs版本号，并更新GPG文件

--- update_gpg.sh
+++ update_gpg.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Linux
-find /usr/share/emacs -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+VERSION=`(emacs --version | awk -F ' ' 'NR==1 {print $3}')`
+GPG_VERSION=26.3
 
-# MacOS
-# find /System/Applications/Emacs.app/Contents/Resources/etc -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+if [ $(echo "$VERSION < $GPG_VERSION" | bc) -eq 1 ]; then
+    # Linux
+    echo "update the new GPG file"
+    find /usr/share/emacs/$VERSION -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+
+    # MacOS
+    # find /System/Applications/Emacs.app/Contents/Resources/etc -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+
+fi
