569a9e4045ddd0f05dc5c7b1defffdb1b98a3775
wylin <wylin.hit@gmail.com>
2019-11-21 11:21:54 +0800

    fix update_gpg.sh file

--- mirror.sh
+++ mirror.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+os=`uname -a`
+macOS="Darwin"
+
+elpa="elpa.gnu.org\/packages"
+tuna_elpa="mirrors.tuna.tsinghua.edu.cn\/elpa\/gnu"
+
+melpa="melpa.org\/packages"
+tuna_melpa="mirrors.tuna.tsinghua.edu.cn\/elpa\/melpa"
+
+orgmode="orgmode.org\/elpa"
+tuna_orgmode="mirrors.tuna.tsinghua.edu.cn\/elpa\/org"
+
 # 修改为国内的镜像，加快下载emacs库的速度
-sed 's/\/\/elpa.gnu.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/gnu/g;s/\/\/melpa.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/melpa/g;s/\/\/orgmode.org\/elpa/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/org/g' ~/.emacs.d/lisp/init-elpa.el > init-elpa.el
-mv init-elpa.el ~/.emacs.d/lisp/init-elpa.el
+if [[ $os =~ $macOS ]];then
+    echo $macOS
+    sed -i "" "s/$elpa/$tuna_elpa/g;s/$melpa/$tuna_melpa/g;s/$orgmode/$tuna_orgmode/g" `grep -rlE "$elpa|$melpa|orgmode" ~/.emacs.d`
+#   sed -i "" 's/\/\/elpa.gnu.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/gnu/g;s/\/\/melpa.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/melpa/g;s/\/\/orgmode.org\/elpa/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/org/g' `grep -rlE "$elpa_packages|$melpa_packages|$orgmode" ~/.emacs.d`
+ 
+    grep -rlE "$elpa|$melpa|$orgmode" ~/.emacs.d
 
-
-
+else
+    echo $os
+    sed -i "s/$elpa/$tuna_elpa/g;s/$melpa/$tuna_melpa/g;s/$orgmode/$tuna_orgmode/g" `grep -rlE "$elpa|$melpa|orgmode" ~/.emacs.d`
+#    sed -i 's/\/\/elpa.gnu.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/gnu/g;s/\/\/melpa.org\/packages/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/melpa/g;s/\/\/orgmode.org\/elpa/\/\/mirrors.tuna.tsinghua.edu.cn\/elpa\/org/g' `grep -rlE "$elpa_packages|$melpa_packages|$orgmode" ~/.emacs.d`
+    grep -rlE "$elpa|$melpa|$orgmode" ~/.emacs.d
+fi
--- update_gpg.sh
+++ update_gpg.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-VERSION=`(emacs --version | awk -F ' ' 'NR==1 {print $3}')`
-GPG_VERSION=26.3
+VERSION=`(emacs --version | awk -F ' ' 'NR==1 {print $3}' )`
+MINOR_VERSION=`(echo $VERSION | awk -F '.' 'NR==1 {print $1$2}')`
+GPG_VERSION=263
 
-if [ $(echo "$VERSION < $GPG_VERSION" | bc) -eq 1 ]; then
+if [ $(echo "$MINOR_VERSION < $GPG_VERSION" | bc) -eq 1 ]; then
     # Linux
     echo "update the new GPG file"
     find /usr/share/emacs/$VERSION -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
