2b7c11f6fbd7bfb82181e687154f93d98fde14cc
wylin <wylin.hit@gmail.com>
2019-11-21 11:32:32 +0800

    fix update gpg

--- update_gpg.sh
+++ update_gpg.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+os=`uname -a`
+macOS="Darwin"
 
-VERSION=`(emacs --version | awk -F ' ' 'NR==1 {print $3}' )`
-MINOR_VERSION=`(echo $VERSION | awk -F '.' 'NR==1 {print $1$2}')`
-GPG_VERSION=263
 
-if [ $(echo "$MINOR_VERSION < $GPG_VERSION" | bc) -eq 1 ]; then
-    # Linux
-    echo "update the new GPG file"
-    find /usr/share/emacs/$VERSION -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+VERSION=`(emacs --version | awk -F ' ' 'NR==1 {print $3}' | cat -b 1-4)`
+GPG_VERSION=26.3
 
-    # MacOS
-    # find /System/Applications/Emacs.app/Contents/Resources/etc -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
-
+if [ $(echo "$VERSION < $GPG_VERSION" | bc) -eq 1 ]; then
+    if [ [$os =~ $macOS]]; then
+    	# macOS
+	# find /System/Applications/Emacs.app/Contents/Resources/etc -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+    else	
+	# Linux
+	echo "update the new GPG file"
+	find /usr/share/emacs/ -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+    fi
 fi
