71603f6d3993fc9e71a689aacc1c56affcaa28cd
wylin <wylin.hit@gmail.com>
2019-11-21 11:42:08 +0800

    fix 2

--- update_gpg.sh
+++ update_gpg.sh
@@ -2,20 +2,23 @@
 os=`uname -a`
 macOS="Darwin"
 
+VERSION=`(emacs --version | awk -F ' ' 'NR==1 {print $3}' | cut -b 1-4)`
+echo $VERSION
 
-VERSION=`(emacs --version | awk -F ' ' 'NR==1 {print $3}' | cat -b 1-4)`
 GPG_VERSION=26.3
 
-if [ $(echo "$VERSION < $GPG_VERSION" | bc) -eq 1 ]; then
-    if [ [$os =~ $macOS]]; then
-    	echo $macOS
-	find /System/Applications/Emacs.app/Contents/Resources/etc -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
-        echo "update the new GPG file"
+if [ $(echo "$VERSION < $GPG_VERSION" | bc) -eq 0 ]; then
+    exit
+fi
 
-    else
-	# Linux
-	echo $os
-        find /usr/share/emacs/ -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
-        echo "update the new GPG file"
-    fi
+if [[ $os =~ $macOS ]]; then
+    echo $macOS
+    find /System/Applications/Emacs.app/Contents/Resources/etc -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+    echo "update the new GPG file"
+
+else
+    # Linux
+    echo $os
+    find /usr/share/emacs/ -name package-keyring.gpg | xargs sudo cp etc/gnu-elpa.gpg-keyring
+    echo "update the new GPG file"
 fi
